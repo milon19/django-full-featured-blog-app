a1db9ab8acc9e016cba396e94c2a2099ff142c65
milon19 <milon15-1472@diu.edu.bd>
2019-11-13 01:44:09 +0600

    Link Bootstrap & Make a Home Page

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -15,8 +15,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
+
+# Database
+DB_NAME = "django_blog_db"
+DB_USER = "postgres"
+DB_PASSWORD = "milonmahato"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -62,11 +66,16 @@ WSGI_APPLICATION = 'blog_site.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': DB_NAME,
+       'USER': DB_USER,
+       'PASSWORD': DB_PASSWORD,
+       'HOST': '127.0.0.1',
+       'PORT': '5432',
+   }
 }
+
 
 
 # Password validation
