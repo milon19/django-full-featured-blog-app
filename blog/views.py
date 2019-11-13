135f4a2159babed796dc26069a128b0cf307ef60
milon19 <milon15-1472@diu.edu.bd>
2019-11-13 17:19:24 +0600

    Add Post Templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def Home(request):
-    return render(request, 'blog/home.html');
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
