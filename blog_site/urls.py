e7d1224fe73d9f01b80d4f6bd2115518126eb3a2
milon19 <milon15-1472@diu.edu.bd>
2019-11-16 00:18:37 +0600

    Photo resized when upload bt Pillow

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('register/', users_view.register, name='register'),
     path('login/', LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/',users_view.profile, name='profile'),
 ]
 
 
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from blog_site import settings
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, ProfileUpdateForm, UserUpdateForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -13,8 +14,31 @@ def register(request):
             form.save()
             username = form.changed_data.get('username')
             messages.success(request, f'Welcome {username}! Login Here.')
-            return redirect('')
+            messages.warning(request, f'You need to update your profile')
+            return redirect('login')
 
     else:
         form = UserRegistrationForm
     return render(request, 'users/registration.html', {'form':form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+        messages.success(request, f'Your account has been updated.')
+        return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
