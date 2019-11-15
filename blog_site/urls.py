850bded2fa0a6b40f07d9d460132cfe5c5da204e
milon19 <milon15-1472@diu.edu.bd>
2019-11-16 00:09:57 +0600

    Add profile view & form

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('register/', users_view.register, name='register'),
     path('login/', LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name='login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
 
 
