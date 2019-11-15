efb01e7d076b89022a4a0a2bedf3136e82c6e6db
milon19 <milon15-1472@diu.edu.bd>
2019-11-15 23:30:18 +0600

    Add LoginView & fixed-not nevigate to login url if logged in

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -4,10 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as users_view
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/', users_view.register, name='register'),
+
 ]
 
 
--- users/views.py
+++ users/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import UserRegistrationForm
 
 
 def register(request):
-    form = UserRegistrationForm
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.changed_data.get('username')
+            messages.success(request, f'Welcome {username}! Login Here.')
+            return redirect('')
+
+    else:
+        form = UserRegistrationForm
     return render(request, 'users/registration.html', {'form':form})
